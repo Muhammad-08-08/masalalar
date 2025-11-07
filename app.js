eee5cd16e19ef130cd8ddd4a6735d2f900527115
muhammad <muhammadnuraliyev119@gmail.com>
2025-11-07 19:07:35 +0500

    2-misol

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-var removeDuplicates = function (nums) {
-  let left = 0;
-
-  for (let right = 1; right < nums.length; right++) {
-    if (nums[right] !== nums[left]) {
-      left++;
-      nums[left] = nums[right];
-    }
-  }
-
-  return left + 1;
-};
-
-console.log(removeDuplicates(0,0,1,1,1,2,2,3,3,4));
+let count = 0;
+setInterval(() => {
+  count++;
+  console.log(count);
+}, 1000);
